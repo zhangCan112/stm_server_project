6d187a629d96c13cfe3381f5fd2860c5c0f72ad4
zhangcan <zhangcan@xuaoshouyi.com>
2019-07-24 07:42:41 +0800

    init beego api project

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "github.com/zhangCan112/stm_server_project/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
